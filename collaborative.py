a59574b173c914afefbeefa07d538370919a18e4
zechengh <zcheustc@gmail.com>
2019-06-25 20:58:06 -0700

    mnist

--- collaborative.py
+++ collaborative.py
@@ -12,3 +12,44 @@ import torch.optim as optim
 import torch.backends.cudnn as cudnn
 
 from net import *
+
+def main():
+
+    DATASET = 'MNIST'
+
+    print "DATASET: ", DATASET
+    if DATASET == 'MNIST':
+
+        mu = torch.tensor([0.5, 0.5, 0.5], dtype=torch.float32)
+        sigma = torch.tensor([0.5, 0.5, 0.5], dtype=torch.float32)
+        Normalize = transforms.Normalize(mu.tolist(), sigma.tolist())
+        Unnormalize = transforms.Normalize((-mu / sigma).tolist(), (1.0 / sigma).tolist())
+
+        tsf = {
+            'train': transforms.Compose(
+            [
+            transforms.ToTensor(),
+            Normalize
+            ]),
+
+            'test': transforms.Compose(
+            [
+            transforms.ToTensor(),
+            Normalize
+            ])
+            }
+
+        trainset = torchvision.datasets.MNIST(root='./data/MNIST', train=True,
+                                        download=True, transform = tsf['train'])
+
+        testset = torchvision.datasets.MNIST(root='./data/MNIST', train=False,
+                                       download=True, transform = tsf['test'])
+
+if __name__ == '__main__':
+
+    import argparse
+    import sys
+    import traceback
+
+    try:
+        main()
